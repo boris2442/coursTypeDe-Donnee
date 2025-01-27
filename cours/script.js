78d31fb3966d1d0ac5b9b9c2752aa0f678d324ba
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-27 15:14:00 +0100

    text

---
+++ cours/script.js
@@ -0,0 +1,13 @@
+let string="chaine";
+console.log(typeof(string));
+
+let number=42;
+console.log(number);
+
+let boolean=true;
+console.log(boolean);
+
+let object_null=null;
+console.log(object_null);
+
+let array=[1, 2, 3];
